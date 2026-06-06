abe01680c9c366b2d1f17d706cb9a6362efb31f0
Santiago Jose Castilla Ospina <126910931+SCO21@users.noreply.github.com>
2026-06-05 21:11:21 -0500

    lab03

--- homework/pregunta_01.py
+++ homework/pregunta_01.py
@@ -2,7 +2,20 @@
 Escriba el codigo que ejecute la accion solicitada en cada pregunta.
 """
 
-# pylint: disable=import-outside-toplevel
+import re
+import glob
+import os
+import pandas as pd
+
+def read_records_from_input(input_folder):
+        sequence = []
+        path = os.path.join(input_folder, "*")
+        files = glob.glob(path)
+        for file in files:
+            with open(file, "r", encoding="utf-8") as f:
+                for line in f:
+                    sequence.append(line)
+        return sequence
 
 
 def pregunta_01():
@@ -16,5 +29,73 @@ def pregunta_01():
     - Las palabras clave deben estar separadas por coma y con un solo
       espacio entre palabra y palabra.
 
-
     """
+    input_folder = "files/input"
+    records = read_records_from_input(input_folder)
+    del records[3] 
+    patron = r"  [a-zA-ZáéíóúÁÉÍÓÚñÑ]"
+    indices_filas = []
+    indices_filas.append(0)
+    indices_filas.extend([coincidencia.start()+2 for coincidencia in re.finditer(patron, records[0])])
+    
+    resultado = []
+    fila_resultado =[]
+
+    for fila in records:
+      if not re.search(r'[a-zA-ZáéíóúÁÉÍÓÚñÑ]', fila.strip())  and len(fila_resultado) > 0:
+        resultado.append(fila_resultado)
+        fila_resultado = []
+
+      else:
+        partes = []
+        for i in range(len(indices_filas)):
+          if i+1 < len(indices_filas):
+            palabras = fila[indices_filas[i]:indices_filas[i+1]].strip()
+          else:
+            palabras = fila[indices_filas[i]:].strip()
+          palabras = re.sub(r"\s+", " ", palabras)
+          partes.append(palabras)
+          
+        if fila_resultado:
+          fila_resultado = list(map(lambda a, b: re.sub(r"\s+", " ", a +" "+b), fila_resultado, partes))
+        else:
+          fila_resultado = partes
+
+    tabla = pd.DataFrame(resultado[1:],columns=resultado[0])
+
+    tabla.columns = (
+      tabla.columns
+      .str.lower()
+      .str.strip()
+      .str.replace(' ', '_')
+    )
+
+    tabla[tabla.columns[-1]] = (
+    tabla[tabla.columns[-1]]
+    .str.replace(',', ', ')
+    .str.replace('.', '')
+    )
+
+    tabla[tabla.columns] = tabla[tabla.columns].apply(
+      lambda col: col.str.replace(r'\s+', ' ', regex=True).str.strip()
+    )
+
+
+
+    tabla[tabla.columns[0]] = pd.to_numeric(tabla[tabla.columns[0]], errors='coerce')
+    tabla[tabla.columns[1]] = pd.to_numeric(tabla[tabla.columns[1]], errors='coerce')
+
+    tabla[tabla.columns[2]] = (
+    tabla[tabla.columns[2]]
+    .str.replace('%', '')
+    .str.replace(' ', '')
+    .str.replace(',', '.')
+    )
+
+
+    tabla[tabla.columns[2]] = pd.to_numeric(tabla[tabla.columns[2]], errors='coerce')
+    return tabla
+
+pregunta_01()
+
+
